88d53ba95df9c93975ced0f5fc662c642f349aed
celmore25@gmail.com <celmore25@gmail.com>
2020-11-28 10:47:53 -0600

    pypi test

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='easyds',
-      version='0.0.2',
+setup(name='easydstools',
+      version='0.0.1',
       description='Data Science toolkit I use a lot',
       url='https://github.com/celmore25/clay-ds/',
       author='Clay Elmore',
       author_email='celmore25@gmail.com',
       license='Apache 2.0',
-      packages=['easyds'],
+      packages=['easydstools'],
       include_package_data=True,
       scripts=[],
       zip_safe=False)
