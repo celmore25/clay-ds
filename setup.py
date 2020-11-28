f0df61ba78efc5759816d0ac8b267a319d6e0115
celmore25@gmail.com <celmore25@gmail.com>
2020-11-28 11:30:13 -0600

    doc updates

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='easydstools',
-      version='0.0.2',
+      version='0.0.3',
       description='Data Science toolkit I use a lot',
       url='https://github.com/celmore25/clay-ds/',
       author='Clay Elmore',
